1e106720900b680ebda2392f2c205913a60b6af3
Prakhar Verma <unstoppableprakhar@gmail.com>
2024-03-22 19:57:13 -0400

    added searchbar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,35 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:to_do/constants/colors.dart';
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        backgroundColor: tdBGColor,
-        elevation: 0,
-         title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: 
-         [
-          Icon(Icons.menu,color: tdBGColor,size: 30,
-          ), 
+    return Scaffold(
+      backgroundColor: tdBGColor,
+      appBar: _BuildAppBar(),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: Column(children: [
           Container(
-            height: 40,
-            width: 40,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.asset('assets/images/profile.jpg'),
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(20)),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(0),
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: tdBlack,
+                  size: 20,
+                ),
+              ),
             ),
           )
-         ],
-         )
-        ),
-      body: Container(
-        child: Text('This is home page!'),
+        ]),
       ),
     );
+  }
+
+  AppBar _BuildAppBar() {
+    return AppBar(
+        backgroundColor: tdBGColor,
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              Icons.menu,
+              color: tdBGColor,
+              size: 30,
+            ),
+            Container(
+              height: 40,
+              width: 40,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset('assets/images/profile.jpg'),
+              ),
+            )
+          ],
+        ));
   }
 }
