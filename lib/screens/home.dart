119e1055b6baff10c6bfd4186ac8b8d26dfde906
Prakhar Verma <unstoppableprakhar@gmail.com>
2024-03-22 21:11:26 -0400

    added new task bas in bottom

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,32 +13,70 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(), // Corrected method name to camelCase
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-        child: Column(children: [
-          searchBox(),
-          Expanded(
-            child: ListView(
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(top: 50, bottom: 20),
-                  child: const Text(
-                    'All Tasks',
-                    style: TextStyle(
-                      color: tdBlack,
-                      fontSize: 30,
-                      fontWeight: FontWeight.w500,
+      body: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Column(children: [
+              searchBox(),
+              Expanded(
+                child: ListView(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(top: 50, bottom: 20),
+                      child: const Text(
+                        'All Tasks',
+                        style: TextStyle(
+                          color: tdBlack,
+                          fontSize: 30,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
                     ),
-                  ),
+                    for (ToDo todoo in todoList)
+                      ToDoItem(
+                        todo: todoo,
+                      ),
+                  ],
                 ),
-                for (ToDo todoo in todoList)
-                  ToDoItem(
-                    todo: todoo,
-                  ),
+              ),
+            ]),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Expanded(
+                    child: Container(
+                        margin: EdgeInsets.only(
+                          bottom: 20,
+                          left: 20,
+                          right: 20,
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 5),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Colors.grey,
+                              offset: Offset(0.0, 0.0),
+                              blurRadius: 10.0,
+                              spreadRadius: 0.0,
+                            ),
+                          ],
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Add a new task',
+                          ),
+                        )))
               ],
             ),
           ),
-        ]),
+        ],
       ),
     );
   }
